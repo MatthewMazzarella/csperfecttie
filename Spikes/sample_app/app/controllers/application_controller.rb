4faf3e239ce65ff5421ef8d4bf154986c55bfb54
Joseph <mangas.j619@gmail.com>
2017-04-26 15:56:04 -0400

    spikes and maintenance guide

---
+++ Spikes/sample_app/app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def hello
+  	render text: "hello, world!"
+  end
+  def contact
+  end
+end
---
+++ Spikes/toy_app/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+	has_many :microposts
+		#svalidates name, presence: true	#Replace FILL_IN eith the right code.
+		#validates FILL_IN, presence: true	#Replace FILL_IN eith the right code.
+end
